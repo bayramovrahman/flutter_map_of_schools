efa5e2028c7bf15104096ae76e8c25b10fba2366
Rahman Bayramov <bayramovrahman@gmail.com>
2024-08-16 15:18:44 +0500

    Internet operation added

--- lib/cubits/cubit_map_service/map_service_cubit.dart
+++ lib/cubits/cubit_map_service/map_service_cubit.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:e_mekdep_school_maps/models/school_model.dart';
 import 'package:e_mekdep_school_maps/services/school_service.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hive/hive.dart';
 
 part 'map_service_state.dart';
 part 'map_service_cubit.freezed.dart';
@@ -19,24 +20,25 @@ class MapServiceCubit extends Cubit<MapServiceState> {
   final SchoolService _schoolService = SchoolService();
 
   Future<void> _fetchSchools() async {
-    try {
-      emit(const MapServiceState.loading());
+    final List<ConnectivityResult> _connectivityResult =  await (Connectivity().checkConnectivity());
+    final _isConnected = _connectivityResult.contains(ConnectivityResult.none);
 
+    if (_isConnected) {
       if (_schoolsBox.isNotEmpty) {
         final _schoolsFromHive = _schoolsBox.values.toList();
         emit(MapServiceState.loaded(schools: _schoolsFromHive));
       } else {
-        _schoolsBox.clear();
-        final schools = await _schoolService.fetchSchools();
-        await _schoolsBox.addAll(schools);
-        emit(MapServiceState.loaded(schools: schools));
+        emit(
+          const MapServiceState.errorMsg(
+            errorMsg: "Internet birikmesi ýok",
+          ),
+        );
       }
-    } catch (e, s) {
-      emit(
-        MapServiceState.errorMsg(
-          errorMsg: 'Serwerde näsazlyk ýüze çykdy! $s',
-        ),
-      );
+    } else {
+      _schoolsBox.clear();
+      final _fetchSchools = await _schoolService.fetchSchools();
+      await _schoolsBox.addAll(_fetchSchools);
+      emit(MapServiceState.loaded(schools: _fetchSchools));
     }
   }
 }
